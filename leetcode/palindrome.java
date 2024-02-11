4865c1dbf97ffaf35abcccf56ee3b36a91c1e898
Sasank Lakimsetti <saisasank.lakimsetti@gmail.com>
2024-02-11 22:15:14 +0530

    self improvement

---
+++ leetcode/palindrome.java
@@ -0,0 +1,48 @@
+/*
+Given an integer x, return true if x is a 
+palindrome
+, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+*/
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        boolean palindrome=false;
+        String str=String.valueOf(x);
+        char[] s=str.toCharArray();
+        int n=s.length;
+        int count=0;
+        if(x>=0&&x<=9) palindrome=true;
+        else if(x<-2147483648 && x>2147483647) palindrome=false;
+        else{
+            for(int i=0;i<n/2;i++){
+                if(s[i]==s[n-i-1]) count++;;
+            }
+            if(count==n/2) palindrome=true;
+            else palindrome=false;
+        }
+        return palindrome;
+    }
+}
