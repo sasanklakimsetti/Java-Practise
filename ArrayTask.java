41c38cbd0df8161782228dcd0ae66636ed095a69
Sasank Lakimsetti <saisasank.lakimsetti@gmail.com>
2024-02-20 21:01:16 +0530

    updated code

--- ArrayTask.java
+++ ArrayTask.java
@@ -6,11 +6,24 @@ import java.util.Scanner;
 //print the majority num from the array(the number getting repeated more than n/2 times)
 public class ArrayTask {
     //approach 2
-    //time complexity O(1)
+    //time complexity O(n logn) since sort method in java uses merge sort, sc: O(1)
     static void majority(int[] arr){
         Arrays.sort(arr);
         int mid=(arr.length-1)/2;
         System.out.println(arr[mid]);
+    }
+    //TC: O(N), SC: O(10^8*N)  --->> O(N)
+    //the problem with this approach is that we are wasting the space unnecessarily
+    static void majority2(int[] arr){
+        int[] freq=new int[10000001];
+        for(int i=0;i<arr.length;i++){
+            freq[arr[i]]++;
+        }
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]>=(arr.length-1)/2)
+                System.out.println(i);
+        }
+        return;
     }
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
@@ -32,7 +45,7 @@ public class ArrayTask {
                 if(arr[i]==arr[j])
                     count++;
             }
-            if(count==n/2){
+            if(count>=n/2){
                 max=arr[i];
                 break;
             }
@@ -42,5 +55,7 @@ public class ArrayTask {
         System.out.println("\nThe num getting repeated n/2 times is "+max);
         System.out.print("The num getting repeated n/2 times using approach 2 is ");
         majority(arr);
+        System.out.print("The num getting repeated n/2 times using approach 3 is ");
+        majority2(arr);
     }
 }
