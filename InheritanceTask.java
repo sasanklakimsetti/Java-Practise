244364484fedfe7822b11d0184332aba20e252fa
Sasank Lakimsetti <saisasank.lakimsetti@gmail.com>
2024-02-26 20:46:59 +0530

    practice on inheritance in java

---
+++ InheritanceTask.java
@@ -0,0 +1,84 @@
+//make a parent class named "Shape" with attributes no.of sides
+//two methods : calculateArea(), calculatePerimter(), printNumberOfSides()
+//create two more class Triangle, Square
+//ask for side and calculate area and perimeter and print no.of sides
+
+import java.util.Scanner;
+import java.lang.Math;
+//parent class
+class Shape{
+    Scanner sc=new Scanner(System.in);
+    int sides;
+    void calculateArea(){
+        System.out.println("The area is ");
+    }
+    void calculatePerimeter(){
+        System.out.println("The perimeter is ");
+    }
+    void printSides(){
+        System.out.println("No.of sides is ");;
+    }
+}
+
+//child 1
+class Triangle extends Shape{
+    int a;
+    int b;
+    int c;
+    Triangle(int a, int b, int c){
+        this.a=a;
+        this.b=b;
+        this.c=c;
+    }
+    void calculateArea(){
+        double s=(a+b+c)/2;
+        double area=Math.sqrt(s*(s-a)*(s-b)*(s-c));
+        System.out.println("Area of the triangle with side is: "+ area);
+    }
+    void calculatePerimeter(){
+        System.out.println("Perimeter of the triangle is: "+(a+b+c));
+    }
+    void printSides(){
+        System.out.println("No.of sides: 3");
+    }
+}
+
+//child 2
+class Circle extends Shape{
+    int r;
+    Circle(int r){
+        this.r=r;
+    }
+
+    @Override
+    void calculateArea() {
+        System.out.println("Area of circle is: "+3.14*r*r);
+    }
+
+    @Override
+    void calculatePerimeter() {
+        System.out.println("Perimeter of circle is: "+2*3.14*r);
+    }
+
+    @Override
+    void printSides() {
+        System.out.println("No.of sides: 0");
+    }
+}
+
+
+public class InheritanceTask {
+    public static void main(String[] args){
+        //calling an object of Triangle
+        Triangle t=new Triangle(1,2,3);
+        t.calculateArea();
+        t.calculatePerimeter();
+        t.printSides();
+
+        //calling an object  of Circle
+        Circle c=new Circle(10);
+        c.calculateArea();
+        c.calculatePerimeter();
+        c.printSides();
+    }
+}
