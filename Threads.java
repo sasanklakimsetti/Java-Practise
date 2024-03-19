038879b0bf97542ab5fe81426aaadae629b3bbd4
sasanklakimsetti <saisasank.lakimsetti@gmail.com>
2024-03-19 20:54:51 +0530

    Threads implementation in Java

--- Threads.java
+++ Threads.java
@@ -1,14 +1,38 @@
 //Threads can be created in two methods
 //1.Class Thread (java in-built class)
-//2. implement Runnable
+//2. implement Runnable (java in-built interface)
 
-public class Threads extends Thread{
-    public void run(){
-        System.out.println("Child thread");
-    }
+//way 1 using Thread Class
+//public class Threads extends Thread{
+//    public void run(){
+//        System.out.println("Child thread");
+//    }
+//    public static void main(String[] args){
+//        Threads t=new Threads();
+//        t.start();  //threads will start
+//        System.out.println("Main thread");
+//        //whenever a thread is created. first the main thread will be executed and then the child will be executed.
+//
+//    }
+//}
+
+//way 2 using Runnable Interface
+//Runnable is actually a functional Interface
+//public class Threads implements Runnable{
+//    public void run(){
+//        System.out.println("Child thread");
+//    }
+//    public static void main(String[] args){
+//        Threads t=new Threads();
+//        Thread T=new Thread(t);
+//        T.start();
+//        System.out.println("Main thread");
+//    }
+//}
+
+//way 3 using lambda expression
+public class Threads{
     public static void main(String[] args){
-        Threads t=new Threads();
-        t.start();  //threads will start
-        System.out.println("Main thread");
+
     }
 }
