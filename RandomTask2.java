f040efb07cc4ca712bd0125808f32a636905eaca
sasanklakimsetti <saisasank.lakimsetti@gmail.com>
2024-04-22 20:58:35 +0530

    Updated code

--- RandomTask2.java
+++ RandomTask2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //Give character matrix i.e. 2D array containing characters and a string with value.
 //can we make get consecutive values from the character matrix which results into the value of string
 // the movement will be to the adjacent values(left, right, top, bottom) not diagonal ones.
@@ -8,64 +10,35 @@
 //String : Mohit
 //Traversal can be [1,1],[2,1],[2,2],[3,2],[3,3] which results to the value of string "Mohit"
 
-import java.util.Scanner;
-
 public class RandomTask2 {
-//    static boolean check(char[][] arr, String str){
-//        int m=arr.length;
-//        int n=arr[0].length;
-//        char[] strArray=str.toCharArray();
-//        int k=0;
-//        boolean ans=false;
-//        for (int i=0;i<m;i++){
-//            for(int j=0;j<n;j++){
-//                if(arr[i][j]==strArray[k]) {
-//                    ans = true;
-//                    k++;
-//                    if (!(k>arr.length)) {
-//                        if ((arr[i - 1][j] == strArray[k]) && (i > 0)) {
-//                            k++;
-//                            ans = true;
-//                        } else if (arr[i + 1][j] == strArray[k]) {
-//                            k++;
-//                            ans = true;
-//                        } else if ((arr[i][j - 1] == strArray[k]) && (j > 0)) {
-//                            k++;
-//                            ans = true;
-//                        } else if (arr[i][j + 1] == strArray[k]) {
-//                            k++;
-//                            ans = true;
-//                        } else {
-//                            ans = false;
-//                        }
-//                    }
-//                }
-//                else ans=false;
-//            }
-//        }
-//        return ans;
-//    }
-    // teacher approach
-    static boolean check2(char mat[][], int i, int j, String str, int si){
-        if(i<0||j<0||i>= mat.length||j>=mat[0].length ||mat[i][j]!=str.charAt(si)) return false;
-        char ch=mat[i][j];
-        mat[i][j]='0';
-        boolean ans=false;
-        ans=ans||check2(mat, i+1,j,str,si+1);
-        ans=ans||check2(mat, i-1,j,str,si+1);
-        ans=ans||check2(mat, i,j+1,str,si+1);
-        ans=ans||check2(mat, i,j-1,str,si+1);
-        mat[i][j]=ch;
+    // TC : O(4^n)
+    static boolean check(char mat[][],int i,int j,String str,int si){
+        if(si >= str.length())return true;
+        if(i < 0 || j < 0 || i >= mat.length || j >= mat[0].length || mat[i][j] != str.charAt(si))return false;
+
+        char ch = mat[i][j];
+        mat[i][j] = '0';
+        boolean ans = false;
+        ans = ans || check(mat, i+1,j,str,si+1);
+        ans = ans || check(mat, i-1,j,str,si+1);
+        ans = ans || check(mat, i,j+1,str,si+1);
+        ans = ans || check(mat, i,j-1,str,si+1);
+        mat[i][j] = ch;
+
         return ans;
     }
-    static boolean check2(char mat[][], String str){
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[0].length;j++){
-                if(mat[i][j]==str.charAt(0)){
-                    if(check2(mat,i,j,str,0)) return true;
+
+    static boolean check(char mat[][],String str){
+
+        for(int i = 0;i<mat.length;i++){
+            for(int j = 0;j<mat[0].length;j++){
+                if(mat[i][j] == str.charAt(0)){
+                    if(check(mat,i,j,str,0))return true;
                 }
             }
         }
+
+        return false;
     }
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
@@ -87,7 +60,7 @@ public class RandomTask2 {
 //            }
 //            System.out.println();
 //        }
-        boolean ans=check2(arr,str);
+        boolean ans=check(arr,str);
         if(ans) System.out.println("The pattern exists");
         else System.out.println("The pattern doesn't exist");
     }
